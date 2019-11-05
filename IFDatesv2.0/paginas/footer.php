3f2840464c7a60aa5875f050ef912d0cda07814e
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-11-05 10:12:39 -0300

    Update footer.php

--- IFDatesv2.0/paginas/footer.php
+++ IFDatesv2.0/paginas/footer.php
@@ -1,8 +1,29 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+<html>
+    <head>
+        <title>IFDates</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/normaliza.css">
+        <link rel="stylesheet" href="../css/responsivo.css">
+        <link rel="stylesheet" href="../css/estilo_php.css">
+    </head>
+    <body>
+        <footer>
+            <div class="row rodape">
+                <div class="col quatro espaco ">
+                  
+                </div>
+                <div class="col quatro redes ">
+                    Twitter: @ifdates </br>
+                    Instagram: @ifdates </br>
+                    Fale conosco: ifdates@gmail.com </br> </br> 
+                    <a href="sobre.php" class="link2" > Sobre o site </a> </br> </br> </br> 
+                    2019
+                </div>
+               
+            </div>
+        </footer>
+    </body>
+</html>
