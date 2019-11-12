5d4c8983fc5a5111f5d8ce794e353d7498971fde
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-11-12 11:08:30 -0300

    Update index.php

--- IFDatesv2.0/paginas/index.php
+++ IFDatesv2.0/paginas/index.php
@@ -1,103 +1,55 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>IFDates</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/normaliza.css">
-        <link rel="stylesheet" href="../css/responsivo.css">
-        <link rel="stylesheet" href="../css/new_style.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <div class="header">
-                    Bem Vindo ao IFDates!
-                </div>
-            </div>
+<?php include_once 'header.php'; ?>
+<div class="row">
+    <div class="col doze">
+        <img src="../imagens/soufundo.jpg"
+             alt="projetos.png"
+             class="capa_sol">
+    </div>
+</div>
+<div class="row">
+    <div class="col doze">
+        <div class="parte1">
+            <h2>Se você quer conhecer os melhores projetos do IFsul Câmpus Camaquã, você está no lugar certo!</h2>
         </div>
-        <div class="row">
-            <div class="col um">
-                <div class="menu">
-                    <a href="login.php" class="link"> Login </a>
-                </div>
-            </div>
-            <div class="col um">
-                <div class="menu">
-                    <a href="cadastro.php" class="link"> Cadastre-se </a>
-                </div>
-            </div>
-            <div class="col um">
-                <div class="menu">
-                    <a href="sobre.php" class="link"> Sobre o site </a>
-                </div>
-            </div>
-            <div class="col nove">
-                <div class="menu">
+    </div>
+</div>
+<div class="row parte2">
+    <div class="espaco">
+        
+    </div>
+    <div class="col oito ">
+        <p class="introducao"> 
+            Aqui você encontrará um ótimo calendário interativo, 
+            além de poder conhecer os melhores projetos do IFSul Câmpus Camaquã! Se você olhar aqui em baixo,
+            verá as imagens clicáveis que te levarão diretamente para o menu de projetos, ou para o calendário,
+            basta você se cadastrar para acompanhar o conteúdo mais de perto! Corre lá, é de graça!</p>
+    </div>
+   
+    <div class="col dois">
+        <a href="calendario.php"> 
+            <img src="../imagens/calendario.png"
+                 alt="calendario.png"
+                 class="imagem"
+                 height="200" width="225">       
+        </a> 
+    </div>
 
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="parte1">
-                    <h2>Se você quer conhecer os melhores projetos do IFsul Câmpus Camaquã, você está no lugar certo!</h2>
-                </div>
-            </div>
-        </div>
-        <div class="row parte2">
-            <div class="col doze">
-                <div class="row">
-                    <div class="col doze">
+    <div class="col dois">
+         <a href="menu_projetos.php"> 
+            <img src="../imagens/projetos.png"
+                 alt="projetos.png"
+                 class="imagem"
+                 height="200" width="225">       
+        </a>  
+    </div>
+</div>
 
-                        <p class="introducao"> 
-                            Aqui você encontrará um ótimo calendário interativo, 
-                            além de poder conhecer os melhores projetos do IFSul Câmpus Camaquã! Se você olhar aqui em baixo,
-                            verá as imagens clicáveis que te levarão diretamente para o menu de projetos, ou para o calendário.
-                            Você também pode se cadastrar para acompanhar o conteúdo mais de perto! Corre lá, é de graça!</p>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col dois espaco">
-
-                    </div>
+<div class="row espaco">
+    
+</div>
+<?php include_once 'footer.php'; ?>
 
 
-                    <div class="col dois">
-                        <a href="menu_projetos.php"> 
-
-                            <img src="../imagens/projetos.png"
-                                 alt="projetos.png"
-                                 class="imagem"
-                                 height="200" width="225">       
-
-                        </a>  
-                    </div>
-                    <div class="col dois espaco">
-
-                    </div>
-                    <div class="col dois espaco">
-
-                    </div>
-                    <div class="col dois espaco">
-                        <a href="calendario.php"> 
-
-                            <img src="../imagens/calendario.png"
-                                 alt="calendario.png"
-                                 class="imagem"
-                                 height="200" width="225">       
-
-                        </a> 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <script src="../scripts/funcoes.js"></script>
-    </body>
+<script src="../scripts/funcoes.js"></script>
+</body>
 </html>
