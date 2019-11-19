3d06841f269e11282f89b13d5dd877faf80b2c65
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-11-19 10:08:10 -0300

    Update header.php

--- IFDatesv2.0/paginas/header.php
+++ IFDatesv2.0/paginas/header.php
@@ -9,31 +9,40 @@
         <link rel="stylesheet" href="../css/normaliza.css">
         <link rel="stylesheet" href="../css/responsivo.css">
         <link rel="stylesheet" href="../css/estilo_php.css">
+        <link rel="stylesheet" href="../css/new_style.css">
+        <link rel="stylesheet" href="../css/seg_estilo.css">
     </head>
-    <body>
+    <body class="fundo_login">
         <div class="row">
             <div class="col um">
-                <div class="menu">
-                    <a href="login.php" class="link"> Login </a>
+                <div class="menuh">
+                    <a href="login.php" class="link_h"> Login </a>
                 </div>
             </div>
             <div class="col um">
-                <div class="menu">
-                    <a href="cadastro.php" class="link"> Cadastre-se </a>
+                <div class="menuh">
+                    <a href="cadastro.php" class="link_h"> Cadastre-se </a>
                 </div>
             </div>
-            <div class="col um">
-                <div class="menu">
-                    <a href="sobre.php" class="link"> Sobre o site </a>
+            <div class="col dois">
+                <div class="menuh">
+                    <a href="sobre.php" class="link_h"> Conheça os desenvolvedores </a>
                 </div>
             </div>
-            <div class="col oito">
-                <div class="menu">
+            <div class="col seis">
+                <div class="menuh">
 
                 </div>
             </div>
-            <div class="col um imagem">
-                <a href="sobre.php"> 
+            <div class="col um">
+                <div class="menuh">
+                <div class="link_h">
+                IFDates
+                </div>
+                </div>
+            </div>
+            <div class="col um imagemh">
+                <a href="index.php"> 
 
                             <img src="../imagens/erased.png"
                                  alt="logo.png"
@@ -42,5 +51,3 @@
                 </a> 
             </div>
         </div>
-    </body>
-</html>
