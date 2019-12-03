b5d348b86916293c007123c4c510d8c0d4a29adb
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-03 10:28:34 -0300

    Update login.php

--- IFDatesv2.0/paginas/login.php
+++ IFDatesv2.0/paginas/login.php
@@ -4,96 +4,89 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <title>Login</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/normaliza.css">
-        <link rel="stylesheet" href="../css/responsivo.css">
-        <link rel="stylesheet" href="../css/new_style.css">
-    </head>
-    <body class="fundo_login">
+<?php include_once 'header.php'; ?>
+<div class="row parte2">
+    <div class="row espaco">
 
-        <div class="row">
+    </div>
+    <div class="row">
 
-            <div class="col quatro">
-                <div class="coluna_um">
-                </div>
-            </div>
+        <div class="col quatro espaco">
 
-            <div class="col quatro">
-                <div class="titulo_login">Faça Login!</div>
-            </div>
         </div>
 
-        <div class="row">
-            <div class="col quatro coluna_um">
-
-            </div>
-
-            <div class="col quatro">
-                <section>
-                    <label for="id_matricula" class="login_mat">Matrícula</label> 
-                    <input id="id_matricula"
-                           maxlength="60"
-                           placeholder="Digite sua matrícula..."
-                           type="text"
-                           name="matricula"
-
-                           required />
-                </section>
-            </div>
+        <div class="col quatro">
+            <div class="titulo_login">Login</div>
         </div>
-        <div class="row">
-            <div class="col quatro coluna_um">
+    </div>
 
-            </div>
-            <div class="col quatro">
-                <section class="fundo">
-                    <label for="id_senha" class="login_senha">Senha</label>
-                    <input id="id_senha"
-                           placeholder="Digite sua senha..."
-                           type="password"
-                           name="senha"
-                           required /> 
-                </section>
-            </div>
-        </div>   
-        <div class="row">
-            <div class="col quatro coluna_um">
+    <div class="row">
+        <div class="col quatro coluna_um">
 
-            </div>
-            <div class="col quatro">
-                <section>
-                    <input class="button1"
-                           type="reset" 
-                           name="redefinir"
-                           value="Redefinir"
-                           onclick="limpalogin()"
-                           />
+        </div>
+
+        <div class="col quatro">
+            <section>
+                <label for="id_matricula" class="login_mat">Matrícula</label> 
+                <input id="id_matricula2"
+                       maxlength="60"
+                       placeholder="Digite sua matrícula..."
+                       type="text"
+                       name="matricula"
+
+                       required />
+            </section>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col quatro coluna_um">
+
+        </div>
+        <div class="col quatro">
+           
+                <label for="id_senha" class="login_senha">Senha</label>
+                <input id="id_senha2"
+                       placeholder="Digite sua senha..."
+                       type="password"
+                       name="senha"
+                       required /> 
+            
+        </div>
+    </div>   
+    <div class="row">
+         
+        
+        <div class="col quatro coluna_um">
+
+        </div>
+        <div class="col quatro">
+            <section>
+                <input class="buttonl"
+                       type="reset" 
+                       name="redefinir"
+                       value="Redefinir"
+                       onclick="limpalogin()"
+                       />
+
+
+                <input class="button"
+                       type="submit"
+                       name="Enviar"
+                       value="Enviar"
+                       onclick="login()"
+                       /> 
+            </section>
+        </div>
+    </div>
+    <div class="row espaco">
       
-                    
-                    <input class="button2"
-                           type="submit"
-                           name="Enviar"
-                           value="Enviar"
-                           onclick="login()"
-                           /> 
-                </section>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col quatro coluna_um">
-
-            </div>
-
-            <div class="col quatro voltar">
-                <a href="index.php" class="link"> Voltar</a>
-            </div>
-        </div>
-
-     <script src="../scripts/funcoes.js"></script>
-    </body>
+    </div>
+    <div class="row espaco">
+      
+    </div>
+</div>
+<script src="../scripts/funcoes.js"></script>
+<?php include_once 'footer.php'; ?>
+</body>
 </html>
 
