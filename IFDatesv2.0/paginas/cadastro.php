d784f34d8227a9ade3f5173a6985ef4700cb92a0
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-03 09:37:45 -0300

    Update cadastro.php

--- IFDatesv2.0/paginas/cadastro.php
+++ IFDatesv2.0/paginas/cadastro.php
@@ -4,160 +4,125 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <title>Cadastro</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/normaliza.css">
-        <link rel="stylesheet" href="../css/responsivo.css">
-        <link rel="stylesheet" href="../css/estilo.css">
- 
- 
-    </head>
-    <body>
+<?php include_once 'header.php'; ?>
+<div class="row ">
+    <div class="col doze capa_mp">
+        Cadastro
+    </div>
+    <div class="row parte2">
+        <div class="row espaco">
 
-        <header class="cabecalho">
-            <h1 class="cabecalho2"> Cadastro </h1>
-        </header>
-        
-        <div class="row">
-            <div class="col dois">
-                <div class="color">
-                    
-                    <p class="menu">Menu</p>
+        </div>
 
-                    <nav class="itens">
-                        <a href="index.php">Página inicial</a>
-                    </nav>
+        <div class="col quatro espaco">
 
-                    <nav class="itens">
-                        <a href="sobre.php">Sobre o site</a>
-                    </nav>
+        </div>
 
-                    
-                    <div class="resto">
-                        
-                    </div>
-                
-                </div>
-                
-            </div>
-            
-            <div class="col oito">
-                
-                <section class="fundo2"> 
-                    <h2 class="titulo">
-                        Faça seu cadastro!
-                    </h2>
-                    <div class="texto">
-                        Com este cadastro você poderá ver todos os eventos e encontros de projetos do IFSul Câmpus Camaquã, além de poder 
-                        conhecer melhor esses projetos.
-                    </div>
-                </section>
-                
-                <form id="id_formulario" name="id_formulario" action="inserir.php" method="post" onsubmit="testeform">
+        <div class="col tres">
+
+            <form id="id_formulario" name="id_formulario" action="inserir.php" method="post" onsubmit="testeform">
                 <section class="fundo">
-                    <label for="id_nome" class="titulo">Nome completo</label><br/>  
+                    <label for="id_nome" class="titulo"></label> </br>
                     <input id="id_nome"
+                           class="campo"
                            maxlength="60"
                            placeholder="Nome completo..."
                            type="text"
                            name="nome"
                            onkeypress="return Onlychars(event)"
-                           
+
 
                            required />
                 </section>
-                
+
                 <section class="fundo">
-                <label for="id_email" class="titulo">Email</label><br/>
-                <input id="id_email"
-                       placeholder="Insira seu email..."
-                       type="email"
-                       name="campo_email" 
-                       required /> 
+                    <label for="id_email" class="titulo"></label></br>
+                    <input id="id_email"
+                           class="campo"
+                           placeholder="Email..."
+                           type="email"
+                           name="campo_email" 
+                           required /> 
                 </section>
-                
+
                 <section class="fundo">
-                    <label for="id_matricula" class="titulo">Matrícula</label></br> 
+                    <label for="id_matricula" class="titulo"></label> </br>
                     <input id="id_matricula"
+                           class="campo"
                            maxlength="60"
-                           placeholder="Digite sua matrícula..."
+                           placeholder="Matrícula..."
                            type="text"
                            name="matricula"
 
                            required />
                 </section>
-                
-               
-                
-                <section class="fundo">
-                <label for="id_senha" class="titulo">Senha</label><br/>
+        </div>
+        <div class="col tres">
+            <section class="fundo">
+                <label for="id_idade" class="titulo"></label> </br>
+                <input id="id_idade"
+                       class="campo"
+                       maxlength="60"
+                       placeholder="Data de nascimento..."
+                       type="text"
+                       name="idade"
+
+                       required />
+            </section>
+
+
+
+            <section class="fundo">
+                <label for="id_senha" class="titulo"></label></br>
                 <input id="id_senha"
-                       placeholder="Mínimo 6 caracteres..."
+                       class="campo"
+                       placeholder="Senha..."
                        onblur="checaTamanho(this)"
                        type="password"
                        name="senha"
                        oninput="checaTamanho(el)"
                        required /> 
-                </section>
-                
-                <section class="fundo">
-                <label for="id_confsenha" class="titulo">Confirme sua senha</label><br/>
+            </section>
+
+            <section class="fundo">
+                <label for="id_confsenha" class="titulo"></label> </br>
                 <input id="id_confsenha"
-                       placeholder="Mínimo 6 caracteres..."
+                       class="campo"
+                       placeholder="Confirme a senha..."
                        type="password"
                        name="confsenha"
                        required /> 
-                </section>
-                
-                <section class="fundo">
-                
-                    <input class="button"
+            </section>
+        </div>
+        <div class="row">
+            
+        <div class="col quatro espaco">
+            
+        </div>
+        
+            
+            <div class="col dois ">
+                <input class="button"
                        type="reset" 
                        name="redefinir"
                        value="Redefinir"/>
-                
-                 <input type="submit"
+            </div>
+                <div class="col um espaco">
+                    
+                </div>
+            <div class="col dois">
+                <input class="button"
+                       type="submit"
                        value="Enviar" 
                        /> 
-                 
-                </section>
-                
-            </form>
-            <div class="row">
-                    <section class="fundo2">
-
-                        <div class="rodape2">
-
-                        </div>
-                    </section>
             </div>
-            <div class="row">
-                    <section class="fundo2">
+            
+        </div>
+        <div class="row espaco2">
 
-                        <div class="rodape2">
+        </div>
 
-                        </div>
-                    </section>
-            </div>
-            <div class="row">
-                    <section class="fundo2">
-
-                        <div class="rodape_cadastro">
-
-                        </div>
-                    </section>
-            </div>
-            </div>
-            <div class="col dois">
-                    <div class="direita">
-                        
-                    </div>
-            </div>
-            </div>
-       <script src="../scripts/funcoes.js"></script>
-    </body>
-</html>
-     
+    </div>
+</div>
+<script src="../scripts/funcoes.js"></script>
+<?php include_once 'footer.php'; ?>
