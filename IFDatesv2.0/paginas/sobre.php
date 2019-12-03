db67045066951481f7750acee08aa59be4e91554
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-03 11:09:41 -0300

    Update sobre.php

--- IFDatesv2.0/paginas/sobre.php
+++ IFDatesv2.0/paginas/sobre.php
@@ -4,109 +4,103 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <title>Sobre o IFDates</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../css/normaliza.css">
-        <link rel="stylesheet" href="../css/responsivo.css">
-        <link rel="stylesheet" href="../css/estilo.css">
-    </head>
-    <body>
-            <header class="cabecalho">
-                <div class="cabecalho2">Sobre o site</div>
-            </header>
-        
-        <div class="row" >
+<?php include_once 'header.php'; ?>
+
+<div class="row parte2" >
+    <div class="col doze capa_mp">
+        Sobre o site
+    </div>
+    <div class="row espaco">
+
+    </div>
+    <div class="col dois espaco">
+
+    </div>
+
+    <div class="col oito">
+
+        <h2 class="titulo">
+            Resumo
+        </h2>
+        <div class="texto">
+            Este site foi desenvolvido para fins acadêmicos, com o intuito de
+            prover informações sobre os eventos e projetos realizados no IFSul Campus 
+            Camaquã.
+        </div>
+    </div>
+
+    <div class="row">
+
+
+
+        <div class="row">
+            <div class="col dois espaco">
+            </div> 
+            <h2 class="titulo">
+                Desenvolvedores:
+            </h2>
+        </div>
+
+        <div class="row">
+            <div class="col dois espaco">
+            </div> 
             <div class="col dois">
-                <div class="color">
-                    <p class="menu">Menu</p>
-
-                    <nav class="itens">
-                        <a href="index.php" class="link"> Página inicial</a>    
-                    </nav>
-
-                    <nav class="itens">
-                    <a href="cadastro.php" class="link"> Cadastre-se</a>
-                    </nav>
-                    <div class="resto">
-                        
-                    </div>
-                    
-                    
-                    
-                </div>
-            </div>
-
-            <div class="col oito">
-                <section class="fundo2"> 
-                    <h2 class="titulo">
-                        Resumo
-                    </h2>
-                    <div class="texto">
-                        Este site foi desenvolvido para fins acadêmicos, com o intuito de
-                        prover informações sobre os eventos e projetos realizados no IFSul Campus 
-                        Camaquã.
-                    </div>
-                </section>
-
-                <div class="row">
-                    <section class="fundo2">
-                        <div>
-                            <h2 class="titulo">
-                                Desenvolvedores:
-                            </h2>
-                        </div>
-                        <div class="texto">
-                            Samara Lellis </br>
-                            Renan Sosa </br>
-                            André Rödel
-                        </div>
-                    </section>
-                </div>
-                
-                <div class="row">
-                    <section class="fundo">
-                        <div>
-                            <h2 class="titulo">
-                                Contatos:
-                            </h2>
-                        </div>
-                        <div class="texto">
-                            samarambaty@gmail.com </br>
-                            renansosa00@gmail.com
-                        </div>
-                        
-                            <img src="../imagens/sonress.png"
-                                 alt="logo1png"
-                                 height="200" width="225">       
-                        
-                    </section>
-                </div> 
-                <div class="row">
-                    <section class="fundo2">
-
-                        <div class="rodape2">
-
-                        </div>
-                    </section>
-                </div>
-                
-                <div class="row">
-                    <section class="fundo2">
-
-                        <div class="rodape">
-
-                        </div>
-                    </section>
-                </div>
+                <img src="../imagens/samara.png"
+                     alt="samara.png"
+                     class="imagem"
+                     height="200" width="200"> 
+                <p class="texto"> Samara Lellis</p>
             </div>
             <div class="col dois">
-                    <div class="direita">
-                        
-                    </div>
+                <img src="../imagens/andre.png"
+                     alt="andre.png"
+                     class="imagem"
+                     height="200" width="200"> 
+                <p class="texto"> André Rödel</p>
+            </div>
+            <div class="col dois">
+                <img src="../imagens/renan.png"
+                     alt="renan.png"
+                     class="imagem"
+                     height="200" width="200"> 
+                <p class="texto"> Renan Sosa</p>
+            </div>
+            <div class="col dois">
+                <img src="../imagens/rafael.png"
+                     alt="rafael.png"
+                     class="imagem"
+                     height="200" width="200"> 
+                <p class="texto"> Rafael Tomkowski</p>
             </div>
         </div>
-    </body>
+
+        <div class="row">
+            <div class="col dois espaco">
+            </div> 
+            <div class="col dois">
+                <h2 class="titulo">
+                    Contatos:
+                </h2>
+
+                <div class="texto">
+                    rafaeltomkowski@gmail.com</br>
+                    andrelimarodel@gmail.com </br>
+                    samarambaty@gmail.com </br>
+                    renansosa00@gmail.com 
+
+                </div>
+            </div>
+
+        </div>
+        <div class="col dois espaco">
+        </div> 
+
+
+
+
+
+    </div>
+</div>
+<?php include_once 'footer.php'; ?>
+</body>
 </html>
