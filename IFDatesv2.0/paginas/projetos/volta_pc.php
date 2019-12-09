0159cace9bef360a742620fa944e5b32a765f4c0
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:08:38 -0300

    Update volta_pc.php

--- IFDatesv2.0/paginas/projetos/volta_pc.php
+++ IFDatesv2.0/paginas/projetos/volta_pc.php
@@ -1,41 +1,28 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Volta PC</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/capas/capa_voltapc.png"
-                     alt ="voltapc.png"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/capas/capa_voltapc.png"
+                 alt ="voltapc.png"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="voltapc_body">
-                    <div class="titulo_cubo">
-                        Volta PC: 
+    </div>
 
-                    </div>
-                    <div class="texto_cubo">
 
-                        Este projeto propõe uma ação de extensão que visa promover a destinação adequada ao lixo eletrônico e ainda a integração entre estudantes da rede pública municipal de Camaquã e o IFSul. São propostas duas ações para destinação do lixo eletrônico: reuso na montagem de novas máquinas ou encaminhamento para reciclagem. Os estudantes arredam lixo eletrônico no campus, realizam a triagem de peças para a montagem de novas máquinas que serão doadas à uma instituição pública municipal com objetivo de facilitar o acesso de jovens à Tecnologia da Informação. Está previsto também o suporte para manutenção dos laboratórios de informática das instituições. Após a entrega das máquinas, os estudantes de Informática ministram um minicurso com as principais informações sobre a utilização dos recursos disponíveis.Com isso, integram conhecimentos do curso, promovem ações sustentabilidade, além de contribuir para um espaço de integração e aproximação do IFSul com a comunidade externa do Bairro vizinho.
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Volta PC: 
 
-                    </div>
-                </div>
-            </div>
         </div>
+        <div class="texto_cubo">
 
-    </body>
-</html>
+            Este projeto propõe uma ação de extensão que visa promover a destinação adequada ao lixo eletrônico e ainda a integração entre estudantes da rede pública municipal de Camaquã e o IFSul. São propostas duas ações para destinação do lixo eletrônico: reuso na montagem de novas máquinas ou encaminhamento para reciclagem. Os estudantes arredam lixo eletrônico no campus, realizam a triagem de peças para a montagem de novas máquinas que serão doadas à uma instituição pública municipal com objetivo de facilitar o acesso de jovens à Tecnologia da Informação. Está previsto também o suporte para manutenção dos laboratórios de informática das instituições. Após a entrega das máquinas, os estudantes de Informática ministram um minicurso com as principais informações sobre a utilização dos recursos disponíveis.Com isso, integram conhecimentos do curso, promovem ações sustentabilidade, além de contribuir para um espaço de integração e aproximação do IFSul com a comunidade externa do Bairro vizinho.
+
+        </div>
+        <div class="espaco"></div>
+    </div>
+
+
+</body>
+<?php include_once 'footer.php'; ?></html>
