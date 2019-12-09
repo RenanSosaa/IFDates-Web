524af7cf67d507b5bc1ec48471b9586f56f645c1
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:01:26 -0300

    Update menu_projetos.php

--- IFDatesv2.0/paginas/menu_projetos.php
+++ IFDatesv2.0/paginas/menu_projetos.php
@@ -5,152 +5,143 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 
-    <?php include_once 'header.php';?>
+<?php include_once 'header.php'; ?>
 
 
-        <div class="row ">
-            <div class="col doze capa_mp">
-                 Menu de Projetos 
-            </div>
-            <div class="row espaco">
-                
-            </div>
-
-
-            <section>
-                <div class="col um espaco">
-                    
-                </div>
-
-                <div class="col dois">
-                    <div class="meios">
-                        <a href="projetos/cubo.php">
-                            <img src="../imagens/projetos/cubo.jpg"
-                                 alt="cubo.jpg"
-                                 height="200" width="225">
-                        </a>
-                    </div>
-                    <div class="row">
-                        <div class="fundo_meios">
-                            <a href="projetos/qualifica.php">
-                                <img src="../imagens/projetos/qualifica.jpg"
-                                     alt ="qualifica.jpg"
-                                     height="200" width="225">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <div class="col dois">
-                    <div class="meios">
-                        <a href="projetos/top_leitor.php">
-                            <img src="../imagens/projetos/topleitor.jpg"
-                                 alt ="topleitor.jpg"
-                                 height="200" width="225">
-                        </a>
-                    </div>
-                    <div class="row">
-                        <div class="fundo_meios">
-                            <a href="projetos/contacao.php">
-                                <img src="../imagens/projetos/contacao.jpg"
-                                     alt ="contacao.jpg"
-                                     height="200" width="225">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <div class="col dois">
-                    <div class="meios">
-                        <a href="projetos/volta_pc.php">
-                            <img src="../imagens/projetos/voltapc.png"
-                                 alt ="voltapc.png"
-                                 height="200" width="225">
-                        </a>
-                    </div>
-                    <div class="row">
-                        <div class="fundo_meios">
-                            <a href="projetos/ifgurantes.php">
-                                <img src="../imagens/projetos/ifgurantes.jpg"
-                                     alt ="ifigurantes.jpg"
-                                     height="200" width="225">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <div class="col dois">
-                    <div class="meios">
-                        <a href="projetos/cine.php">
-                            <img src="../imagens/projetos/cine.jpg"
-                                 alt ="cine.jpg"
-                                 height="200" width="225">
-                        </a>
-                    </div>
-                    <div class="row">
-                        <div class="fundo_meios">
-                            <a href="projetos/astromusica.php">
-                                <img src="../imagens/projetos/astro.jpg"
-                                     alt ="astromusica.jpg"
-                                     height="200" width="225">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <div class="col dois">
-                    <div class="meios">
-                        <a href="projetos/tabus.php">
-                            <img src="../imagens/projetos/tabus.jpg"
-                                 alt ="tabus.jpg"
-                                 height="200" width="225">
-                        </a>
-                    </div>
-                    <div class="row">
-                        <div class="fundo_meios">
-                            <a href="projetos/cafe.php">
-                                <img src="../imagens/projetos/cafe.jpg"
-                                     alt ="cafe.jpg"
-                                     height="200" width="225">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section>
-                <div class="um">
-                    <div class="direitas">
-
-                    </div>
-                </div>
-            </section>
-
-        </div>
-
-        <div class="row ">
-            <div class="col doze">
-                <div class="an_footer">
-
-                </div>
-            </div>
-        </div>
+<div class="row ">
+    <div class="col doze capa_mp">
+        Menu de Projetos 
+    </div>
+    <div class="row parte2">
         <div class="row espaco">
-                
+
         </div>
 
-       
-        <?php include_once 'footer.php';?>
+
+        <section>
+            <div class="col um espaco">
+
+            </div>
+
+            <div class="col dois">
+                <div class="meios">
+                    <a href="cubo.php">
+                        <img src="../imagens/projetos/cubo.jpg"
+                             alt="cubo.jpg"
+                             height="200" width="225">
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="fundo_meios">
+                        <a href="qualifica.php">
+                            <img src="../imagens/projetos/qualifica.jpg"
+                                 alt ="qualifica.jpg"
+                                 height="200" width="225">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="col dois">
+                <div class="meios">
+                    <a href="top_leitor.php">
+                        <img src="../imagens/projetos/topleitor.jpg"
+                             alt ="topleitor.jpg"
+                             height="200" width="225">
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="fundo_meios">
+                        <a href="contacao.php">
+                            <img src="../imagens/projetos/contacao.jpg"
+                                 alt ="contacao.jpg"
+                                 height="200" width="225">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="col dois">
+                <div class="meios">
+                    <a href="volta_pc.php">
+                        <img src="../imagens/projetos/voltapc.png"
+                             alt ="voltapc.png"
+                             height="200" width="225">
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="fundo_meios">
+                        <a href="ifgurantes.php">
+                            <img src="../imagens/projetos/ifgurantes.jpg"
+                                 alt ="ifigurantes.jpg"
+                                 height="200" width="225">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="col dois">
+                <div class="meios">
+                    <a href="cine.php">
+                        <img src="../imagens/projetos/cine.jpg"
+                             alt ="cine.jpg"
+                             height="200" width="225">
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="fundo_meios">
+                        <a href="astromusica.php">
+                            <img src="../imagens/projetos/astro.jpg"
+                                 alt ="astromusica.jpg"
+                                 height="200" width="225">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="col dois">
+                <div class="meios">
+                    <a href="tabus.php">
+                        <img src="../imagens/projetos/tabus.jpg"
+                             alt ="tabus.jpg"
+                             height="200" width="225">
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="fundo_meios">
+                        <a href="cafe.php">
+                            <img src="../imagens/projetos/cafe.jpg"
+                                 alt ="cafe.jpg"
+                                 height="200" width="225">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="row espaco">
+
+        </div>
+
+
+    </div>
+
+
+</div>
 
 
 
-    </body>
+
+<?php include_once 'footer.php'; ?>
+
+
+
+</body>
 </html>
