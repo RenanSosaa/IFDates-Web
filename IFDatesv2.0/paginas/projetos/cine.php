606e8ae5f3d2e18c81d22e9ec6b71bb8cd69015f
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:05:22 -0300

    Update cine.php

--- IFDatesv2.0/paginas/projetos/cine.php
+++ IFDatesv2.0/paginas/projetos/cine.php
@@ -1,50 +1,35 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Cine & Debate</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/capas/capa_cine.jpg"
-                     alt ="cine.jpg"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/capas/capa_cine.jpg"
+                 alt ="cine.jpg"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="cine_body">
-                   <div class="titulo_cubo">
-                        Cine+Debate: 
-                    </div>
-                    <div class="texto_cubo">
-                        
-
-                        É um projeto que visa a discussão de certos assuntos contidos em filmes, os quais, muitas vezes passam despercebidos pelo espectador. 
-
-                        Os encontros geralmente ocorrem nas terças-feiras e quintas-feiras, com horário variando entre 18:45 e 19:15.
-                        <div class="texto_cubo">
-                        Alguns filmes já assistidos e discutidos:
-                        </div>
-                        <ul>
-                            <li>Blade Runner</li>
-                            <li>Matrix</li> 
-                            <li>Com Amor, Simon</li> 
-                            <li>Bohemian Rhapsody</li>
-                        </ul>
-                    </div> 
-                </div>
-            </div>
+    </div>
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Cine+Debate: 
         </div>
-    </body>
-</html>
+        <div class="texto_cubo">
+
+
+            É um projeto que visa a discussão de certos assuntos contidos em filmes, os quais, muitas vezes passam despercebidos pelo espectador. 
+
+            Os encontros geralmente ocorrem nas terças-feiras e quintas-feiras, com horário variando entre 18:45 e 19:15.
+            <div class="texto_cubo">
+                Alguns filmes já assistidos e discutidos:
+            </div>
+            <ul>
+                <li>Blade Runner</li>
+                <li>Matrix</li> 
+                <li>Com Amor, Simon</li> 
+                <li>Bohemian Rhapsody</li>
+            </ul>
+        </div> 
+        <div class=" espaco"></div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?></html>
