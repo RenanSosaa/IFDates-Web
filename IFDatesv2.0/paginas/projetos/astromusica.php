85d468ffdcbb45b65260a1b568e131d69348e923
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:03:56 -0300

    Update astromusica.php

--- IFDatesv2.0/paginas/projetos/astromusica.php
+++ IFDatesv2.0/paginas/projetos/astromusica.php
@@ -1,40 +1,29 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Astromúsica</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/projetos/astro.jpg"
-                     alt ="astro.jpg"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/projetos/astro.jpg"
+                 alt ="astro.jpg"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="contacao_body">
-                    <div class="titulo_cubo">
-                        Astromusica:
+    </div>
+    
+   
+    
+    <div class="row parte2">
+         <div class="row espaco"></div>
+                <div class="titulo_cubo">
+                    Astromusica:
 
-                    </div>
-                    <div class="texto_cubo">
-
-                        O Astromusica é um evento que busca relacionar a música e a astronomia, incentivando também a participação da comunidade exterior. É um evento que compreende em parte a comunidade de fora, e a comunidade de dentro.
-
-                    </div>
                 </div>
-            </div>
-        </div>
-    </body>
-</html>
+                <div class="texto_cubo">
+
+                    O Astromusica é um evento que busca relacionar a música e a astronomia, incentivando também a participação da comunidade exterior. É um evento que compreende em parte a comunidade de fora, e a comunidade de dentro.
+
+                </div>
+        
+        <div class="row espaco"></div>
+    </div>
+</body
+<?php include_once 'footer.php'; ?>
+
