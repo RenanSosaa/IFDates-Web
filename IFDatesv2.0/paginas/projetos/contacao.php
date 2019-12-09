12a7b5958de75447cdab9e82a697120dbf0b1a6a
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:05:48 -0300

    Update contacao.php

--- IFDatesv2.0/paginas/projetos/contacao.php
+++ IFDatesv2.0/paginas/projetos/contacao.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Contação de Histórias</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/capas/capa_contacao.png"
-                     alt ="contacao.png"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/capas/capa_contacao.png"
+                 alt ="contacao.png"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="contacao_body">
-                    <div class="titulo_cubo">
-                        Contação: 
-                    </div>
-                    <div class="texto_cubo">
+    </div>
 
-
-                        É um projeto que visa contar histórias para crianças de escolas vulneráveis. Incentivando a leitura e o uso da imaginação.
-
-                    </div>
-                </div>
-            </div>
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Contação: 
         </div>
-    </body>
-</html>
+        <div class="texto_cubo">
+
+
+            É um projeto que visa contar histórias para crianças de escolas vulneráveis. Incentivando a leitura e o uso da imaginação.
+
+        </div>
+        <div class="espaco"></div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?>
