08553b12eb0b73f9dcc2b8adb5fb323c55cc4890
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:07:13 -0300

    Update qualifica.php

--- IFDatesv2.0/paginas/projetos/qualifica.php
+++ IFDatesv2.0/paginas/projetos/qualifica.php
@@ -1,40 +1,26 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Qualifica</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/capas/capa_qualifica.png"
-                     alt ="qualifica.png"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/capas/capa_qualifica.png"
+                 alt ="qualifica.png"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="qualifica_body">
-                    <div class="titulo_cubo">
-                        Qualifica:
-                    </div>
-                    <div class="texto_cubo">
+    </div>
 
-
-                        Este projeto propõe uma ação de ensino e extensão, que visa promover a criação de um site e um aplicativo. O “QualIFica” tem como objetivo  auxiliar alunos de baixa renda que finalizaram o ensino fundamental e pretendem ingressar em uma instituição federal. O projeto visa  ampliar o número de ingressantes de baixa renda nos institutos federais, promovendo uma rede de comunicação mais efetiva entre os propensos ingressos com a abrangência de 100% dos conteúdos abordados nas matrizes curriculares dos respectivos institutos. A motivação para este projeto se deu devido a um problema recorrente enfrentado pelos vestibulandos: a dificuldade em encontrar os conteúdos abordados no vestibular de forma clara e coesa em uma única ferramenta. Devido a isso, o projeto contou com a colaboração de professores do Instituto Federal - Campus Camaquã, nas áreas de exatas e humanas, a fim de elaborar as apostilas com confiabilidade e segurança. O QualIFica foi desenvolvido por alunos do curso técnico em informática, o que proporciona uma excelente oportunidade de pôr em prática os conhecimentos da área vistos em aula, além de promover a integração de diferentes áreas do conhecimento.
-
-                    </div>
-                </div>
-            </div>
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Qualifica:
         </div>
-    </body>
-</html>
+        <div class="texto_cubo">
+
+
+            Este projeto propõe uma ação de ensino e extensão, que visa promover a criação de um site e um aplicativo. O “QualIFica” tem como objetivo  auxiliar alunos de baixa renda que finalizaram o ensino fundamental e pretendem ingressar em uma instituição federal. O projeto visa  ampliar o número de ingressantes de baixa renda nos institutos federais, promovendo uma rede de comunicação mais efetiva entre os propensos ingressos com a abrangência de 100% dos conteúdos abordados nas matrizes curriculares dos respectivos institutos. A motivação para este projeto se deu devido a um problema recorrente enfrentado pelos vestibulandos: a dificuldade em encontrar os conteúdos abordados no vestibular de forma clara e coesa em uma única ferramenta. Devido a isso, o projeto contou com a colaboração de professores do Instituto Federal - Campus Camaquã, nas áreas de exatas e humanas, a fim de elaborar as apostilas com confiabilidade e segurança. O QualIFica foi desenvolvido por alunos do curso técnico em informática, o que proporciona uma excelente oportunidade de pôr em prática os conhecimentos da área vistos em aula, além de promover a integração de diferentes áreas do conhecimento.
+
+        </div>
+        <div class="espaco"></div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?>
