73f0d8ff23056fee169ee4de08d9609700507820
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:07:40 -0300

    Update tabus.php

--- IFDatesv2.0/paginas/projetos/tabus.php
+++ IFDatesv2.0/paginas/projetos/tabus.php
@@ -1,41 +1,26 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Tabus</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col doze">
-                <img src="../../imagens/capas/capa_tabus.png"
-                     alt ="tabus.png"
-                     class="capa">     
-            </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col doze">
+            <img src="../imagens/capas/capa_tabus.png"
+                 alt ="tabus.png"
+                 class="capa">     
         </div>
-        <div class="row">
-            <div class="col doze">
-                <div class="tabus_body">
-                    <div class="titulo_cubo">
-                        Tabus:
-                    </div>
-                    <div class="texto_cubo">
+    </div>
 
-
-                        É um projeto que visa abrir espaço para que os alunos falem sobre assuntos que são tabus na sociedade. Esses, que muitas vezes tem grande importância e pouca atenção dentro do âmbito escolar.
-
-                    </div>
-                </div>
-            </div>
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Tabus:
         </div>
+        <div class="texto_cubo">
 
-    </body>
-</html>
+
+            É um projeto que visa abrir espaço para que os alunos falem sobre assuntos que são tabus na sociedade. Esses, que muitas vezes tem grande importância e pouca atenção dentro do âmbito escolar.
+
+        </div>
+        <div class=" espaco"></div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?>
