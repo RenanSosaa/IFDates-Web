525ea102529221f18bfa272dac773f13b9150405
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 13:33:16 -0300

    Update perfil.php

--- IFDatesv2.0/paginas/perfil.php
+++ IFDatesv2.0/paginas/perfil.php
@@ -17,23 +17,10 @@
     <div class="col dois espaco">
 
     </div>
-<div class="col dois espaco"></div>
+    <div class="col dois espaco"></div>
     <div class="col tres">
-        
+
         <form id="id_formulario" name="id_formulario" action="inserir.php" method="post" onsubmit="testeform">
-            <section class="fundo">
-                <label for="id_nome" class="titulo"></label> </br>
-                <input id="id_nome"
-                       class="campo"
-                       maxlength="60"
-                       placeholder="Nome completo..."
-                       type="text"
-                       name="nome"
-                       onkeypress="return Onlychars(event)"
-
-
-                       required />
-            </section>
 
             <section class="fundo">
                 <label for="id_email" class="titulo"></label></br>
@@ -45,55 +32,20 @@
                        required /> 
             </section>
 
-            <section class="fundo">
-                <label for="id_matricula" class="titulo"></label> </br>
-                <input id="id_matricula"
-                       class="campo"
-                       maxlength="60"
-                       placeholder="Matrícula..."
-                       type="text"
-                       name="matricula"
-
-                       required />
-            </section>
     </div>
-    <div class="col tres">
-        <section class="fundo">
-            <label for="id_idade" class="titulo"></label> </br>
-            <input id="id_idade"
-                   class="campo"
-                   maxlength="60"
-                   placeholder="Data de nascimento..."
-                   type="text"
-                   name="idade"
 
-                   required />
-        </section>
+    <section class="fundo">
+        <label for="id_senha" class="titulo"></label></br>
+        <input id="id_senha"
+               class="campo"
+               placeholder="Senha..."
+               onblur="checaTamanho(this)"
+               type="password"
+               name="senha"
+               oninput="checaTamanho(el)"
+               required /> 
+    </section>
 
-
-
-        <section class="fundo">
-            <label for="id_senha" class="titulo"></label></br>
-            <input id="id_senha"
-                   class="campo"
-                   placeholder="Senha..."
-                   onblur="checaTamanho(this)"
-                   type="password"
-                   name="senha"
-                   oninput="checaTamanho(el)"
-                   required /> 
-        </section>
-
-        <section class="fundo">
-            <label for="id_confsenha" class="titulo"></label> </br>
-            <input id="id_confsenha"
-                   class="campo"
-                   placeholder="Confirme a senha..."
-                   type="password"
-                   name="confsenha"
-                   required /> 
-        </section>
-    </div>
     <div class="row">
 
         <div class="col quatro espaco">
@@ -122,7 +74,4 @@
 </div>
 
 
-
-
-
-<?php include_on
+<?php include_once 'footer.php'; ?>
