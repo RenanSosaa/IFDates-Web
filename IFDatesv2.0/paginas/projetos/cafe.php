59923ebf13b51d6617fcdaf52c1732c07c083a53
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:04:34 -0300

    Update cafe.php

--- IFDatesv2.0/paginas/projetos/cafe.php
+++ IFDatesv2.0/paginas/projetos/cafe.php
@@ -1,38 +1,31 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Café da Relatividade</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col cinco">
-                <img src="../../imagens/projetos/cafe.jpg"
-                     alt ="cafe.jpg"
-                     class="capa_cafe">   
-            </div>
-            <div class="col sete">
-                <div class="cafe_corpo">
-                    <div class="titulo_cubo">
-                        Café da Relatividade:  
-                    </div>
-                    <div class="texto_cubo">
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col quatro espaco"></div>
 
-
-                        Promove encontros para debater assuntos sobre a Física moderna, geralmente usando algum filme ou série como exemplo. Essas discussões, são sempre acompanhadas de café, então traga sua caneca!
-
-                    </div> 
-                </div>
-            </div>
+        <div class="col cinco">
+            <img src="../imagens/projetos/cafe.jpg"
+                 alt ="cafe.jpg"
+                 class="capa_cafe">   
         </div>
-    </body>
-</html>
+
+        <div class="row parte2">
+            <div class="row espaco"></div>
+
+
+            <div class="titulo_cubo">
+                Café da Relatividade:  
+            </div>
+            <div class="texto_cubo">
+
+
+                Promove encontros para debater assuntos sobre a Física moderna, geralmente usando algum filme ou série como exemplo. Essas discussões, são sempre acompanhadas de café, então traga sua caneca!
+
+            </div> 
+            <div class="row espaco"></div>
+
+        </div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?>
