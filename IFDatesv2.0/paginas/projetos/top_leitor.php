7c1aa59cab8b8d546bb868874b9cdc56b9c71aef
saalelliss <56595942+saalelliss@users.noreply.github.com>
2019-12-09 17:08:08 -0300

    Update top_leitor.php

--- IFDatesv2.0/paginas/projetos/top_leitor.php
+++ IFDatesv2.0/paginas/projetos/top_leitor.php
@@ -1,47 +1,33 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>TOP Leitor</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../../css/normaliza.css">
-        <link rel="stylesheet" href="../../css/responsivo.css">
-        <link rel="stylesheet" href="../../css/pro_estilo.css">
-    </head>
-    <body>
-        <div class="row">
-            <div class="col quatro">
-                <div class="top_capa">
-                    <img src="../../imagens/capas/capa_topleitor.jpg"
-                         alt ="toleitor.jpg"
-                         height="200" width="400">
-                </div>
-            </div>
-            <div class="col oito">
-                <div class="top_cabecalho">
-                    Traçando o Perfil do Leitor
-                </div>
-            </div> 
-        </div>
-
-        <div class="row">
-            <div class="doze">
-                <div class="top_corpo">
-                    <div class="titulo_cubo">
-                        Traçando o Perfil do Leitor:   
-                    </div>
-                    <div class="texto_cubo">
-
-                        O Traçando Perfil do Leitor é um projeto do Câmpus Camaquã, que possui grande êxito na comunidade no que se refere á mediação de leitura e á formação de leitores. A partir de seus resultados, surgiu a demanda de multiplicar as ações do clube do livro no câmpus, que são abertas á toda comunidade, bom como á formação de professores, bibliotecários e demais interessados que trabalhem com as séries finais do Ensino Fundamental ou Ensino Médio.
-
-                    </div>
-                </div>
+<?php include_once 'header.php'; ?>
+<body>
+    <div class="row">
+        <div class="col quatro">
+            <div class="top_capa">
+                <img src="../imagens/capas/capa_topleitor.jpg"
+                     alt ="toleitor.jpg"
+                     height="200" width="400">
             </div>
         </div>
-    </body>
-</html>
+        <div class="col oito">
+            <div class="top_cabecalho">
+                Traçando o Perfil do Leitor
+            </div>
+        </div> 
+    </div>
+
+
+    <div class="parte2">
+        <div class="row espaco"></div>
+        <div class="titulo_cubo">
+            Traçando o Perfil do Leitor:   
+        </div>
+        <div class="texto_cubo">
+
+            O Traçando Perfil do Leitor é um projeto do Câmpus Camaquã, que possui grande êxito na comunidade no que se refere á mediação de leitura e á formação de leitores. A partir de seus resultados, surgiu a demanda de multiplicar as ações do clube do livro no câmpus, que são abertas á toda comunidade, bom como á formação de professores, bibliotecários e demais interessados que trabalhem com as séries finais do Ensino Fundamental ou Ensino Médio.
+
+        </div>
+        <div class="espaco2"></div>
+    </div>
+
+</body>
+<?php include_once 'footer.php'; ?>
